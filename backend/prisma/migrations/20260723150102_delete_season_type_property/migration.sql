1aa4a4fa97c3e71b34ef449db88c9d4dee0aa388
Michael Lee <mlee@MacBook-Pro-3.local>
2026-07-23 11:01:34 -0400

    delete season type property due to duplicate key insertion limitations

---
+++ backend/prisma/migrations/20260723150102_delete_season_type_property/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `season_type` on the `player_stats` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "player_stats" DROP COLUMN "season_type";
