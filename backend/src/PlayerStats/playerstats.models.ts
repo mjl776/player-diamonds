a57ec752eb61ff650383379174f94eb3dd5b1988
Michael Lee <mlee@MacBook-Pro-3.local>
2026-07-26 18:33:43 -0400

    created standard deviation function + calculate games above 2 standard deviations

--- backend/src/PlayerStats/playerstats.models.ts
+++ backend/src/PlayerStats/playerstats.models.ts
@@ -1,3 +1,5 @@
+import { Decimal } from "@prisma/client/runtime/library";
+import { PlayerGameLogs } from "generated/prisma/browser";
 
 // Controller Types
 export type FindUnderValuedPlayersQuery = {
@@ -5,3 +7,33 @@ export type FindUnderValuedPlayersQuery = {
     seasonType: string;
 }
 
+// Service types
+export type StandDeviationResult = {
+    player_id: string;
+    player_name: string;
+    standard_deviation_points: Decimal;
+    standard_deviation_assists: Decimal;
+    standard_deviation_rebounds: Decimal;
+    standard_deviation_offensive_rebounds: Decimal;
+    standard_deviation_defensive_rebounds: Decimal;
+    standard_deviation_minutes: Decimal;
+    standard_deviation_field_goals_made: Decimal;
+    standard_deviation_field_goals_attempted: Decimal;
+    standard_deviation_field_goal_percentage: Decimal;
+    standard_deviation_three_point_made: Decimal;
+    standard_deviation_three_point_attempted: Decimal;
+    standard_deviation_three_point_percentage: Decimal;
+    standard_deviation_free_throws_made: Decimal;
+    standard_deviation_free_throws_attempted: Decimal;
+    standard_deviation_free_throw_percentage: Decimal;
+    standard_deviation_steals: Decimal;
+    standard_deviation_blocks: Decimal;
+    standard_deviation_turnovers: Decimal;
+    standard_deviation_personal_fouls: Decimal;
+    standard_deviation_plus_minus: Decimal;
+}
+
+export type PlayerGameStats = {
+    player_game_logs: PlayerGameLogs,
+    stat_match: string;
+}
