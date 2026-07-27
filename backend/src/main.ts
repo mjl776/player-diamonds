0a7da3edf50704d6df9b2b8171a95f117ac6e61b
Michael Lee <mlee@MacBook-Pro-3.local>
2026-07-27 02:23:55 -0400

    improved query by running on the postgressql side opposed to running on TS clientside

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  BigInt.prototype['toJSON'] = function () {
+    return this.toString();
+  };
   app.enableCors({ origin: ['http://localhost:3000'] });
   await app.listen(process.env.PORT ?? 8080);
 }
