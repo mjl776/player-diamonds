38a34a7b75a8fb2c09b5d424907a320f68ff1146
Michael Lee <mlee@MacBook-Pro-3.local>
2026-07-27 22:40:30 -0400

    troubleshooting railway issue

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,14 @@ import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const port = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   BigInt.prototype['toJSON'] = function () {
     return this.toString();
   };
   app.enableCors({ origin: ['http://localhost:3000', 'https://playerdiamonds.com', 'https://www.playerdiamonds.com'] });
-  await app.listen(process.env.PORT ?? 8080);
+  await app.listen(port, "0.0.0.0");
 }
 bootstrap();
