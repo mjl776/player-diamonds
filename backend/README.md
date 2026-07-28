1a146b412672fc5ddbd67c5ccdb747a016cf7f6c
mjl776 <53539497+mjl776@users.noreply.github.com>
2026-07-28 16:50:30 -0400

    Added position level filtering in backend + updated script to include missed position players (#22)
    Overview of Changes
    This PR adds backend filtering to allow users in the frontend to choose which positions they want to filter on. It also relegates some of the original logic to use Promise.all to optimize faster query times instead of each promise having to be awaited individually.
    On the backend now, the controller now takes an array of strings which will be the positions they choose from the frontend.
    This PR also updates the backend script to include positions that were previously missing in the get league averages scripts, which allows a wider of pool of players to be compared against in the undervalued player finder level
    
    Co-authored-by: Michael Lee <mlee@MacBook-Pro-3.local>

--- backend/README.md
+++ backend/README.md
@@ -4,4 +4,9 @@ Backend for Project
 
 curl -G "http://localhost:8080/find-undervalued-players" \
   --data-urlencode "season=2023-24" \
-  --data-urlencode "seasonType=Regular Season"
+  --data-urlencode "seasonType=Regular Season" \
+  --data-urlencode "positions=G" \
+  --data-urlencode "positions=F" \
+  --data-urlencode "positions=C" \
+  --data-urlencode "positions=F-C" \
+  --data-urlencode "positions=G-F" 
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
 const port = process.env.PORT || 8080;
@@ -10,6 +11,7 @@ async function bootstrap() {
     return this.toString();
   };
   app.enableCors({ origin: ['http://localhost:3000', 'https://playerdiamonds.com', 'https://www.playerdiamonds.com'] });
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(port, "0.0.0.0");
 }
 bootstrap();
